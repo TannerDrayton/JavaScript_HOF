013aea70d007621828ce22e5250cdafe9cc0a810
TannerDrayton <Tanner.drayton01@gmail.com>
2021-01-07 15:51:11 -0600

    solved exercise 1 in two different ways

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function plus(number) {
+	return function (plusNumber) {
+		return (plusNumber += number);
+		//Traditional way of declaring a function inside of a function
+	};
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
+
+console.log("EXERCISE 1:\n==========\n");
+function plus(number) {
+	return (plusNumber) => (plusNumber += number);
+	//This is function arrow notation declaration
+}
+
+let plus25 = plus(25);
+console.log(plus25(10));
